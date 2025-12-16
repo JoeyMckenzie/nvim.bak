66aac5b3094be1704209636d25c4c830acc0ba78
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-12-16 09:19:25 -0800

    feat: add buffer line

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        name_formatter = function(buf)
+          return vim.fn.fnamemodify(buf.path, ":.")
+        end,
+      },
+    },
+  },
+}
