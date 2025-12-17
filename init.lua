abc91cd871a980279a78280b8d1ca910d65f6b87
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-12-16 21:55:19 -0800

    feat: update ai stuff

--- init.lua
+++ init.lua
@@ -1,2 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("codecompanion").setup({
+  extensions = {
+    mcphub = {
+      callback = "mcphub.extensions.codecompanion",
+      opts = {
+        make_vars = true,
+        make_slash_commands = true,
+        show_result_in_chat = true,
+      },
+    },
+  },
+})
