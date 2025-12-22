6159012b5fdf6f22405ac2de528a773cd872d1f6
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-12-22 15:06:20 -0800

    chore: wip

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,17 @@
 return {
   "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      file_ignore_patterns = {
+        "^.git/",
+        "^node_modules/",
+        "^vendor/",
+        ".phpstan.cache/",
+        "^storage/framework/",
+        "^bootstrap/cache/",
+      },
+    },
+  },
   keys = {
     {
       "<leader>fF",
@@ -8,6 +20,7 @@ return {
           hidden = true,
           no_ignore = true,
           no_ignore_parent = true,
+          file_ignore_patterns = { "^.git/" },
         })
       end,
       desc = "Find Files (incl hidden + gitignored)",
--- lua/plugins/testing.lua
+++ lua/plugins/testing.lua
@@ -1,7 +1,7 @@
 return {
   {
     "joeymckenzie/linear.nvim",
-    dir = "/Users/jmckenzie/github.com/joeymckenzie/linear.nvim",
+    dir = "/Users/joeymckenzie/code/github.com/joeymckenzie/linear.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
