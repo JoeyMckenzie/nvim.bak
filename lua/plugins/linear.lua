9314dd464138630960e6e519baa68ee4bd721f71
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-12-23 15:48:21 -0800

    chore: wip

--- lua/plugins/linear.lua
+++ lua/plugins/linear.lua
@@ -1,9 +1,24 @@
 return {
   {
     "joeymckenzie/linear.nvim",
+    dir = "/Users/joeymckenzie/code/github.com/joeymckenzie/linear.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
     },
+    opts = {
+      ui = {
+        statusline = {
+          show_title = true,
+        },
+      },
+    },
+  },
+  -- Add Linear statusline to lualine
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, 1, require("linear").statusline)
+    end,
   },
 }
