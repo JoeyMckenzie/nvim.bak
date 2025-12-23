10426bcad26a482510c7cbd158acb4ea426a52be
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-12-22 23:41:11 -0800

    chore: wip

--- lua/plugins/linear.lua
+++ lua/plugins/linear.lua
@@ -1,7 +1,6 @@
 return {
   {
     "joeymckenzie/linear.nvim",
-    dir = "/Users/joeymckenzie/code/github.com/joeymckenzie/linear.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
